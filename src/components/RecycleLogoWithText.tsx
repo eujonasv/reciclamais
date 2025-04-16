8e227d734a52c28037e3a25ebf6b63a576bc7315
RECICLA+ <vianajonas245@gmail.com>
2025-04-16 00:40:33 -0300

    md

--- src/components/RecycleLogoWithText.tsx
+++ src/components/RecycleLogoWithText.tsx
@@ -42,7 +42,7 @@ const RecycleLogoWithText = ({
       <img 
         src={logoSrc} 
         alt="RECICLA+" 
-        className={`w-auto s=xl}`}
+        className={`w-auto s=md}`}
       />
     </div>
   );
